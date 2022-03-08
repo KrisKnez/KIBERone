2c017e22081dceb164ca7c177658e872a89f43cc
Kristijan <kristijankej@gmail.com>
2022-03-08 17:39:55 +0100

    feat: smoothscroll polifyl

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,16 @@
+import React from "react";
+
 import "/styles/globals.css";
 import "mapbox-gl/dist/mapbox-gl.css";
 
-// import smoothscroll from "smoothscroll-polyfill";
+import smoothscroll from "smoothscroll-polyfill";
 // import React from "react";
 
 function MyApp({ Component, pageProps }) {
-  // React.useLayoutEffect(() => {
-  //   // kick off the polyfill!
-  //   smoothscroll.polyfill();
-  // }, []);
+  React.useEffect(() => {
+    // kick off the polyfill!
+    smoothscroll.polyfill();
+  }, []);
 
   return <Component {...pageProps} />;
 }
