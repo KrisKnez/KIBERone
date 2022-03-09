edad0c8e60cdd2e6621b3aae256c373b560f7991
Kristijan <kristijankej@gmail.com>
2022-03-09 23:40:04 +0100

    feat: fb pixel and google analystics

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,52 @@
 import React from "react";
 
-// import { polyfill } from "smoothscroll-polyfill";
-import 'smoothscroll-anchor-polyfill'
+import Script from "next/script";
+
+// import "smoothscroll-anchor-polyfill";
 
 import "/styles/globals.css";
-import "mapbox-gl/dist/mapbox-gl.css";
+// import "mapbox-gl/dist/mapbox-gl.css";
 
 function MyApp({ Component, pageProps }) {
-  // React.useEffect(() => {
-  //   polyfill();
-  // }, []);
+  return (
+    <>
+      {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
 
-  return <Component {...pageProps} />;
+          gtag('config', 'GA_MEASUREMENT_ID');
+        `}
+      </Script>
+      {/* <!-- Facebook Pixel Code --> */}
+
+      <Script id="facebook-pixel">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window,document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+
+          fbq('init', '3554582134618586'); 
+
+          fbq('track', 'PageView');
+        `}
+      </Script>
+
+      {/* <!-- End Facebook Pixel Code --> */}
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
