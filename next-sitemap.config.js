56cc9036ff2d719ac63dc45d6258d4eca8addb4f
Kristijan <kristijankej@gmail.com>
2022-03-10 23:17:40 +0100

    feat: sitemap and robots.txt generator

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://www.k1-cyber.hr/',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+}
