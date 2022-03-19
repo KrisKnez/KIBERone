222e142a2c609f642a609e9f44319ef11c437dc0
Kristijan <kristijankej@gmail.com>
2022-03-19 23:50:08 +0100

    fix: meta keywords

--- seo.config.js
+++ seo.config.js
@@ -2,14 +2,19 @@ const SEO = {
   title: "KIBERone - IT škola programiranje za djecu",
   description:
     "KIBERone je ustanova koja je kvalificirana od strane Microsoft, Roblox i Samsung korporacija. Najbolja obrazovna ustanova za digitalni odgoj djece u 2018. g. na teritoriju Europske Unije.",
-  keywords:
-    "Programiranje za djecu, Programiranje u Zagrebu, Programiranje Hrvatska, Za djecu u Zagrebu, Tečaj programiranja, Tečaj stranog jezika, Tečajeve programiranja u Zagrebu, Tečajeve kodiranja u Zagrebu, Škola programiranja za djecu, Minecraft, Roblox, Minecraft tečajeve, Roblox tečajeve, Video igre Zagreb, Kodiranje, Coding, Robotika, Radionice u Zagrebu, Besplatne radionice za djecu, Radionice za djecu, Edukacija za djecu u Zagrebu",
   openGraph: {
     type: "website",
     locale: "hr_HR",
     url: "https://www.k1-cyber.hr/",
     site_name: "KIBERone",
   },
+  additionalMetaTags: [
+    {
+      property: "keywords",
+      content:
+        "Programiranje za djecu, Programiranje u Zagrebu, Programiranje Hrvatska, Za djecu u Zagrebu, Tečaj programiranja, Tečaj stranog jezika, Tečajeve programiranja u Zagrebu, Tečajeve kodiranja u Zagrebu, Škola programiranja za djecu, Minecraft, Roblox, Minecraft tečajeve, Roblox tečajeve, Video igre Zagreb, Kodiranje, Coding, Robotika, Radionice u Zagrebu, Besplatne radionice za djecu, Radionice za djecu, Edukacija za djecu u Zagrebu",
+    },
+  ],
 };
 
 export default SEO;
